43efb8dcdda00bc61f4aef756c32cde28cb6f9aa
Vinicius de Souza <viniciusdossantos547@gmail.com>
2025-03-08 21:40:40 +0000

    retorna postgre_version e max_connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
+import { Query } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updated_at = new Date().toISOString();
+
+  const resultVersion = await database.query("SELECT version();");
+  const postgres_version = resultVersion.rows[0].version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections");
+  const max_connections = resultMaxConnections.rows[0].max_connections;
+
+  const resultUsedConnections = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE state = 'activate'",
+  );
+
+  response.status(200).json({
+    updated_at: updated_at,
+    postgres_version: postgres_version,
+    max_connections: max_connections,
+    result_used_connections: resultUsedConnections,
+  });
 }
 
 export default status;
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("Get to /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  const max_connections = Number(responseBody.max_connections);
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.postgres_version).toContain("PostgreSQL");
+  expect(max_connections).toBeGreaterThan(0);
 });
