b42979c1654f326df6917897699270707598ab06
Vinicius de Souza <viniciusdossantos547@gmail.com>
2025-03-11 04:16:48 +0000

    refatorando

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,28 +4,45 @@ import database from "infra/database.js";
 // Importa a classe Query do banco de dados (PostgreSQL), para consultas mais detalhadas
 import { Query } from "pg";
 
+async function creatConnections(numberConnections) {
+  const connections = [];
+
+  for (let i = 0; i < numberConnections; i++) {
+    const connection = database.query("SELECT pg_sleep(3);");
+    connections.push(connection);
+  }
+
+  await Promise.all(connections);
+}
+
+creatConnections(9);
+
 // Função assincrona responsável pelas requests e responses da API
 async function status(request, response) {
   const updated_at = new Date().toISOString(); // Data atual no formato ISO
 
   const resultVersion = await database.query("SELECT version();"); // Query (JSON) da versão do PostgreSQL
-  const postgres_version = resultVersion.rows[0].version; // Versão PostgreSQL extraída da Query
+  const postgresVersion = resultVersion.rows[0].version; // Versão PostgreSQL extraída da Query
 
   const resultMaxConnections = await database.query("SHOW max_connections"); // Query (JSON) máximo de conexões
-  const max_connections = resultMaxConnections.rows[0].max_connections; // Máximo de conexões extraída da Query
+  const maxConnections = resultMaxConnections.rows[0].max_connections; // Máximo de conexões extraída da Query
 
   const resultUsedConnections = await database.query(
     "SELECT COUNT (*) FROM pg_stat_activity WHERE state = 'active'", // Query (JSON) conexões ativas
   );
-  const used_connections = resultUsedConnections.rows[0].count; // Conexões usadas extraídas da Query
+  const usedConnections = resultUsedConnections.rows[0].count; // Conexões usadas extraídas da Query
 
   // Resposta JSON com as informações
   // Status HTTP 200 indica sucesso
   response.status(200).json({
     updated_at: updated_at, // Data atual
-    postgres_version: postgres_version, // Versão PostgreSQL
-    max_connections: max_connections, // Conexões máximas
-    used_connections: used_connections, // Conexões usadas
+    dependencies: {
+      database: {
+        version: postgresVersion, // Versão PostgreSQL
+        max_connections: maxConnections, // Conexões máximas
+        used_connections: usedConnections, // Conexões usadas
+      },
+    },
   });
 }
 
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -4,12 +4,20 @@ test("Get to /api/v1/status", async () => {
   expect(response.status).toBe(200); // Espera que retorne 200
 
   const responseBody = await response.json(); // Define o corpo da resposta como um JSON
+
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Cria uma data para comparar com a data original
-  const max_connections = Number(responseBody.max_connections); // Define o número máximo de conexões
-  const used_connections = Number(responseBody.used_connections); // Define as conexões usadas
+
+  const versionDatabase = responseBody.dependencies.database.version; // Define a versão do banco de dados
+
+  const max_connections = parseInt(
+    responseBody.dependencies.database.max_connections,
+  ); // Define o número máximo de conexões
+  const used_connections = parseInt(
+    responseBody.dependencies.database.used_connections,
+  ); // Define as conexões usadas
 
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // Espera que a data retornada seja igual a criada para comparação
-  expect(responseBody.postgres_version).toContain("PostgreSQL"); // Espera que a versão do banco de dados tenha 'PostgreSQL'
+  expect(versionDatabase).toContain("PostgreSQL"); // Espera que a versão do banco de dados tenha 'PostgreSQL'
   expect(max_connections).toBeGreaterThan(0); // Espera que o máximo de conexões sejam maior que 0
   expect(used_connections).toBeGreaterThan(0); // Espera que as conexões usadas sejam maior que 0
 });
